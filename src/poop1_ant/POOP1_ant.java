ee8c9dc0b1dbc65ce0452446da27b74888f42414
Janet García <143154582+Pao-18@users.noreply.github.com>
2024-09-26 22:14:51 -0600

    Creación práctica 1

---
+++ src/poop1_ant/POOP1_ant.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poop1_ant;
+
+/**
+ *
+ * @author janet
+ */
+public class POOP1_ant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /**
+         * Partes importantes del código
+         */
+        //  No descarga nada en la nuve
+        System.out.println("Hola mundo con ANT");
+        System.out.println("Desde POO G3");
+        /**
+         *  Declaración de variables 
+         */
+        int a= 3; 
+        Integer b= 3; 
+        /**
+         * new: palabra reservada 
+         * String: constructor 
+         */
+        String cadena = new String("hola"); 
+        /**
+         * Impresió de variables 
+         */
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("");
+        
+                
+    }
+    
+}
